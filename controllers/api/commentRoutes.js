d8869f60487e34eb4cbcb71f78992111530ef153
hannahk <hanhannah839@gmail.com>
2023-01-27 00:46:53 -0800

    fixed some issues with routes

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -6,5 +6,7 @@ const withAuth = require('../../utils/auth');
 // TODO - create a POST route for creating a new comment
 // This should be a protected route, so you'll need to use the withAuth middleware
 
+
+
 module.exports = router;
 
